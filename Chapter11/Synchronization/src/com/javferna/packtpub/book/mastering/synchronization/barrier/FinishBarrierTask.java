2655158ec34d3c2e6af2ae96318bae6f96c4c2f9
gabrieldvv <gduvignaud@gmail.com>
2025-08-22 19:43:31 +0200

    Rename chapters

--- Chapter11/Synchronization/src/com/javferna/packtpub/book/mastering/synchronization/barrier/FinishBarrierTask.java
+++
@@ -1,9 +0,0 @@
-package com.javferna.packtpub.book.mastering.synchronization.barrier;
-
-public class FinishBarrierTask implements Runnable {
-
-	@Override
-	public void run() {
-		System.out.println("FinishBarrierTask: All the tasks have finished");
-	}
-}
